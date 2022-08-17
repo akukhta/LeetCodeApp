24444ffe1779a09c0994ab6802009c2fef7e730c
akukhta <electricsix23@gmail.com>
2022-08-17 14:31:55 +0200

    Login has added

--- src/UI/LoginWidget.cpp
+++ src/UI/LoginWidget.cpp
@@ -1,11 +1,33 @@
 #include "LoginWidget.h"
+#include <QFile>
 
 LoginWidget::LoginWidget(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 	ui.label->setPixmap(QPixmap(":/icons/logo.svg"));
+	setWindowFlags(Qt::FramelessWindowHint | Qt::WindowSystemMenuHint);
 }
 
 LoginWidget::~LoginWidget()
 {}
+
+void LoginWidget::on_closeBtn_clicked()
+{
+	QApplication::quit();
+}
+
+void LoginWidget::on_guestBtn_clicked()
+{
+	close();
+}
+
+void LoginWidget::on_loginBtn_clicked()
+{
+	QFile::copy(":/scripts/login", "login.pyw");
+
+	QProcess process;
+	process.start("python.exe", { "login.pyw" });
+	process.waitForFinished();
+
+}
--- src/UI/LoginWidget.h
+++ src/UI/LoginWidget.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QMainWindow>
+#include <qprocess.h>
 #include "ui_LoginWidget.h"
 
 class LoginWidget : public QMainWindow
@@ -11,6 +12,12 @@ public:
 	LoginWidget(QWidget *parent = nullptr);
 	~LoginWidget();
 
+private slots:
+	void on_closeBtn_clicked();
+	void on_guestBtn_clicked();
+	void on_loginBtn_clicked();
+
 private:
 	Ui::LoginWidgetClass ui;
+
 };
